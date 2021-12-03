63f1ed6823080e4381347e783c823ea5c858531c
marine-nag <71370369+marine-nag@users.noreply.github.com>
2021-12-03 02:58:28 +0200

    Update placeholder.js

--- placeholder.js
+++ placeholder.js
@@ -99,14 +99,14 @@ define(function (require) {
                                                     
                                                     var address = scp.order.CustomerInfo.Address;
                                                     
-                                                    var isValidEmail = address.EmailAddress.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+                                                    var isValidEmail = Boolean(address.EmailAddress.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/));
                                                     
                                                      var isValidAddress = address.EmailAddress.length > 1 && isValidEmail && address.Address1.length > 1 && address.Town.length > 1
                                                                 && address.PostCode.length > 1 && (address.Company.length > 1 || address.FullName.length > 1);
                                                     
                                                     address = scp.order.CustomerInfo.BillingAddress;
 
-                                                    var isValidEmailB = address.EmailAddress.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+                                                    var isValidEmailB = Boolean(address.EmailAddress.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/));
 
                                                     var isValidBilling = address.EmailAddress.length > 1 && isValidEmailB && address.Address1.length > 1 && address.Town.length > 1
                                                         && address.PostCode.length > 1 && (address.Company.length > 1 || address.FullName.length > 1);
